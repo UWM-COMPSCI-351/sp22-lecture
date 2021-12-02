74595a51aa62de8e91c47edbd9f3339de7719f7d
boyland <boyland@UWM013414.ad.uwm.edu>
2021-12-02 09:53:14 -0600

    after lecture

--- src/edu/uwm/cs351/util/SortUtils.java
+++ src/edu/uwm/cs351/util/SortUtils.java
@@ -24,7 +24,44 @@ public class SortUtils<T> {
 		}
 	}
 	
+	private int partition(T[] array, int lo, int hi) {
+		T pivot = array[lo];
+		int hole = lo;
+		while (hi - lo > 1) {
+			if (hole == lo) {
+				if (comparator.compare(pivot, array[hi-1]) > 0) {
+					array[hole] = array[hi-1];
+					hole = hi-1;
+				} else {
+					--hi;
+				}
+			} else {
+				if (comparator.compare(pivot, array[lo]) >= 0) {
+					++lo;
+				} else {
+					array[hole] = array[lo];
+					hole = lo;
+				}
+			}
+		}
+		array[hole] = pivot;
+		return hole;
+	}
+	
+	/**
+	 * Sort a range in the array
+	 * @param array
+	 * @param lo inclusive
+	 * @param hi exclusive
+	 */
+	private void qshelp(T[] array, int lo, int hi) {
+		if (hi - lo < 2) return;
+		int pivot = partition(array, lo, hi);
+		qshelp(array, lo, pivot);
+		qshelp(array, pivot+1, hi);
+		// everything is sorted!
+	}
 	public void quickSort(T[] array) {
-		// TODO
+		qshelp(array, 0, array.length);
 	}
 }
